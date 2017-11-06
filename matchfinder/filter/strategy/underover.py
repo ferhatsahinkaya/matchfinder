52be02720d7a7cc911fdd5597402dde819fe68e1
Ferhat Sahinkaya <ferhatsahinkaya@gmail.com>
2017-11-06 21:19:57 +0000

    Make home away a string parameter instead of boolean

--- matchfinder/filter/strategy/underover.py
+++ matchfinder/filter/strategy/underover.py
@@ -27,7 +27,7 @@ def has_probability(team_stats, expected_chance):
 def get_matches(competition, search_filter):
     matches = footballapi.get_next_week_matches(competition['id'])
     filter['halftime'] = search_filter['halffulltime'] == 'halftime'
-    filter['homeaway'] = search_filter['homeaway']
+    filter['homeaway'] = search_filter['homeaway'] == 'on'
     filter['numberofgoals'] = search_filter['numberofgoals']
     filter['chance'] = search_filter['percent']
     filter['competitionId'] = competition['id']
